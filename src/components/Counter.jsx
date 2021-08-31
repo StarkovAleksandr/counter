fee88316fe7b467d352bbdd48b9a4766183bd5ca
StarkovAleksandr <starkovlife@gmail.com>
2021-08-31 19:23:54 +0300

    add hooks into component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,24 +5,26 @@ import minusLogo from '../assets/icons/minus.png';
 import resetLogo from '../assets/icons/reset.png';
 import { useDispatch, useSelector } from 'react-redux';
 
-const dispatch = useDispatch();
-const cash = useSelector((state) => state.cash);
+export const Counter = () => {
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash);
 
-console.log(cash);
+  console.log(cash);
 
-export const Counter = () => (
-  <div className="wrapper">
-    <div className="count">0</div>
-    <div className="wrapper__btn">
-      <button className="inc btn">
-        <img src={plusLogo} alt="plus" />
-      </button>
-      <button className="dec btn">
-        <img src={minusLogo} alt="minus" />
-      </button>
-      <button className="reset btn">
-        <img src={resetLogo} alt="reset" />
-      </button>
+  return (
+    <div className="wrapper">
+      <div className="count">0</div>
+      <div className="wrapper__btn">
+        <button className="inc btn">
+          <img src={plusLogo} alt="plus" />
+        </button>
+        <button className="dec btn">
+          <img src={minusLogo} alt="minus" />
+        </button>
+        <button className="reset btn">
+          <img src={resetLogo} alt="reset" />
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
